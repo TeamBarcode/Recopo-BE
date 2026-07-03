c96b51ab3650c0952142f8bc7bbb90af91047e5a
Tran Thu Trang | 유정 <tranthutrangcppp@gmail.com>
2026-07-03 19:28:54 +0900

    chore: init project

---
+++ src/main/java/com/barcode/recopo/RecopoApplication.java
@@ -0,0 +1,13 @@
+package com.barcode.recopo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecopoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecopoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barcode/recopo/RecopoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barcode.recopo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecopoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
