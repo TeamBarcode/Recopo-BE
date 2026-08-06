30b4634d9c75b5c3b242d48627d5e6b147ba1de2
LeeTaeYoung <leety7248@gmail.com>
2026-08-06 21:19:06 +0900

    Fix API endpoint path in NotificationController

--- src/main/java/com/barcode/recopo/notification/controller/NotificationController.java
+++ src/main/java/com/barcode/recopo/notification/controller/NotificationController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/notifications")
+@RequestMapping("/api/notifications")
 public class NotificationController {
     private final NotificationService notificationService;
 
