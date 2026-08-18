55ab7752aa8d0feef696ea10049b509bdd37d5c0
Tran Thu Trang | 유정 <tranthutrangcppp@gmail.com>
2026-08-18 18:11:53 +0900

    feat: cors setting

--- src/main/java/com/barcode/recopo/global/config/SecurityConfig.java
+++ src/main/java/com/barcode/recopo/global/config/SecurityConfig.java
@@ -58,7 +58,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedOriginPatterns(List.of(
+                "https://recopo-fe.vercel.app",
+                "http://localhost:3000",
+                "http://localhost:5173"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
